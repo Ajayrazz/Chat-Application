813255d933d1753726955f58bf48741d8d102b0b
Ajay Razz <ajayrazz854334@gmail.com>
2024-10-07 20:43:21 +0530

    new changes

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,39 +1,115 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./chat.css"
+import EmojiPicker from 'emoji-picker-react'
 
 const Chat = () => {
-  return (
-    <div className='chat'>
-        <div className="top">
-            <div className="user">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>John Doe</span>
-                    <p>UserDescription : Lorem ipsum dolor sit, amet.</p>
 
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+    const endRef = useRef(null)
+
+    useEffect(() => {
+       endRef.current?.scrollIntoView({behavior:"smooth"}) 
+    },[])
+
+    const handleEmoji = e => {
+        setText((prev) => prev + e.emoji)
+        setOpen(false)
+    }
+
+    return (
+        <div className='chat'>
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>John Doe</span>
+                        <p>UserDescription : Lorem ipsum dolor sit, amet.</p>
+
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt="" />
+                    <img src="./video.png" alt="" />
+                    <img src="./info.png" alt="" />
                 </div>
             </div>
-            <div className="icons">
-                <img src="./phone.png" alt="" />
-                <img src="./video.png" alt="" />
-                <img src="./info.png" alt="" />
+
+
+            {/* ---------------- CENTER ------------------------------- */}
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="text">
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="text">
+                        <img src="https://cdn.pixabay.com/photo/2022/06/11/09/20/snake-7256057_1280.jpg" alt="" />
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div ref={endRef}></div>
+            </div>
+
+
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt="" />
+                    <img src="./camera.png" alt="" />
+                    <img src="./mic.png" alt="" />
+                </div>
+                <input type="text" placeholder='Type a message...' onChange={(e) => setText(e.target.value)} value={text} />
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" onClick={() => setOpen((prev) => !prev)} />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+                    </div>
+                </div>
+                <button className='sendButton'>Send</button>
             </div>
         </div>
-        <div className="center"></div>
-        <div className="bottom">
-            <div className="icons">
-                <img src="./img.png" alt="" />
-                <img src="./camera.png" alt="" />
-                <img src="./mic.png" alt="" />
-            </div>
-            <input type="text" placeholder='Type a message...'/>
-            <div className="emoji">
-                <img src="./emoji.png" alt="" />
-            </div>
-            <button className='sendButton'>Send</button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Chat
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -4,7 +4,77 @@ import "./detail.css"
 const Detail = () => {
   return (
     <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem, ipsum dolor sit amet consect</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
 
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://media.istockphoto.com/id/1268467641/photo/small-portion-salad.jpg?s=1024x1024&w=is&k=20&c=cD5MJ34krzvzEsFJU34o37PzTtr8RRT-jBFk5_-O1mM=" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://media.istockphoto.com/id/1268467641/photo/small-portion-salad.jpg?s=1024x1024&w=is&k=20&c=cD5MJ34krzvzEsFJU34o37PzTtr8RRT-jBFk5_-O1mM=" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://media.istockphoto.com/id/1268467641/photo/small-portion-salad.jpg?s=1024x1024&w=is&k=20&c=cD5MJ34krzvzEsFJU34o37PzTtr8RRT-jBFk5_-O1mM=" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://media.istockphoto.com/id/1268467641/photo/small-portion-salad.jpg?s=1024x1024&w=is&k=20&c=cD5MJ34krzvzEsFJU34o37PzTtr8RRT-jBFk5_-O1mM=" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className='icon'/>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className='logout'>Logout</button>
+      </div>
     </div>
   )
 }
